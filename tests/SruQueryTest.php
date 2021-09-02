6c9e72f7c46988046491535558a90abff746d092
Gerold Ritter <ritter@e-hist.ch>
2021-09-02 15:46:08 +0200

    improve tests

--- tests/SruQueryTest.php
+++ tests/SruQueryTest.php
@@ -16,7 +16,7 @@ class SruQueryTest extends TestCase
 {
     protected $sruquery;
 
-    public function setUp() : void
+    public function setUp(): void
     {
         parent::setUp();
         $this->sruquery = new SruQuery;
@@ -46,17 +46,17 @@ class SruQueryTest extends TestCase
         $date_end = "2000";
         $limit = "50";
         $params = [
-            "query" => 'Serverchoice all "'.$fulltextstring.'" AND isad.reference == "'.$reference.'" AND isad.title === "'.$title.'" AND isad.unknown === "'.$unknown.'" AND isad.date WITHIN "'.$date_start.' '.$date_end.'"',
+            "query" => 'Serverchoice all "' . $fulltextstring . '" AND isad.reference == "' . $reference . '" AND isad.title === "' . $title . '" AND isad.unknown === "' . $unknown . '" AND isad.date WITHIN "' . $date_start . ' ' . $date_end . '"',
             /* "query" => 'Serverchoice%20all%20%22Regierungsrat%22%20AND%20isad.date%20WITHIN%20%221000%202000%22', */
             "maximumRecords" => $limit,
         ];
         $result = $this->sruquery->getQueryParams($params);
         $this->assertEquals(['value' => $fulltextstring, 'operator' => 'AND'], $result['fulltext']);
         $this->assertEquals(['value' => $reference, 'operator' => 'LIKE'], $result['reference']);
-        $this->assertEquals(['value' => $date_start."-01-01", 'operator' => '>='], $result['date_start']);
-        $this->assertEquals(['value' => $date_end."-12-31", 'operator' => '<='], $result['date_end']);
+        $this->assertEquals(['value' => $date_start . "-01-01", 'operator' => '>='], $result['date_start']);
+        $this->assertEquals(['value' => $date_end . "-12-31", 'operator' => '<='], $result['date_end']);
         $this->assertArrayNotHasKey($unknown, $result);
-        $this->assertEquals(['value' => $limit, 'operator' => '='], $result['limit']);
+        $this->assertEquals($limit, $result['limit']);
     }
     public function testAcceptsCustomParams()
     {
@@ -66,7 +66,7 @@ class SruQueryTest extends TestCase
         $customfields = [$customkey => 'key'];
         $customoperators = ["adj" => "near"];
         $params = [
-            "query" => $customkey .' '.$customoperator.' "'.$customvalue .'"',
+            "query" => $customkey . ' ' . $customoperator . ' "' . $customvalue . '"',
         ];
         $result = $this->sruquery->getQueryParams($params, $customfields, $customoperators);
         $this->assertEquals(['value' => $customvalue, 'operator' => 'near'], $result['key']);
